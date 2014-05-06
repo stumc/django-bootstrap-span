3a4460219ade7b1a93c29752a0632e5d7cc2684e
smcmilla <smcmillan@usefulautomation.com>
2014-05-06 18:36:15 -0400

    Added setup.cfg and chnages setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 "Setup file for django-bootstrap-span"
 
 from setuptools import setup
-
+version='0.0.1'
 with open('README.rst') as fh:
     long_description = fh.read()
 
 setup(
       name='django-bootstrap-span',
-      version='0.0.1',
+      version=version,
       description='Django bootstrap span adds class=spanN support in forms input elements',
       long_description=long_description,
       author='Stuart McMillan',
@@ -15,6 +15,8 @@ setup(
       url='https://github.com/stumc/django-bootstrap-span',
       packages=['bootstrap_span','bootstrap_span.templatetags'],
       install_requires=['six','django','django-bootstrap'],
+      keywords = ['django', 'bootstrap', 'class', 'span', 'form', 'input', 'width' ],
+      download_url = 'https://github.com/stumc/django-bootstrap-span/tarball/v'+version,
       classifiers=[
 #        "Development Status :: 5 - Production/Stable",
         'Development Status :: 3 - Alpha',
@@ -22,6 +24,7 @@ setup(
         "Intended Audience :: Developers",
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
